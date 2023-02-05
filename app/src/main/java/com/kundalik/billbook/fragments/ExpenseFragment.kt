00a0dbf1a19299906e52aec3d0c5a0076222f42e
kundalik_suryawanshi <kundaliksofficial@gmail.com>
2023-02-06 00:59:28 +0530

    calculation-by-formulae-complete

--- app/src/main/java/com/kundalik/billbook/fragments/ExpenseFragment.kt
+++ app/src/main/java/com/kundalik/billbook/fragments/ExpenseFragment.kt
@@ -2,6 +2,7 @@ package com.kundalik.billbook.fragments
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,11 +16,20 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.kundalik.billbook.R
 import com.kundalik.billbook.databinding.FragmentExpenseBinding
+import java.lang.Integer.parseInt
 
 
 class ExpenseFragment : Fragment() {
 
     private lateinit var binding: FragmentExpenseBinding
+
+    //fields variables
+    private lateinit var CC: TextInputEditText //current cash expense
+    private lateinit var HC: TextInputEditText //home cash expense
+    private lateinit var BK: TextInputEditText //banking
+    private lateinit var DT: TextInputEditText //shop cash expense
+    private lateinit var DD: TextInputEditText //five percent
+    private lateinit var CI: TextInputEditText //cash in
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,54 +56,127 @@ class ExpenseFragment : Fragment() {
         binding.etProduct14.setAdapter(arrayAdapter)
         binding.etProduct15.setAdapter(arrayAdapter)
 
-
         binding.btnContinue.setOnClickListener {
-
             uploadProductDataToDatabase()
-        }
-        binding.btnCancle.setOnClickListener {
-
-            val dialog = Dialog(requireContext())
-            dialog.setContentView(R.layout.item_expense_dialog)
-            dialog.setCancelable(false)
-            val makeTotal = dialog.findViewById<Button>(R.id.btn_make_total)
-            val saveProductExpense = dialog.findViewById<Button>(R.id.btn_save_expense)
-
-            makeTotal.setOnClickListener {
-                //calculating total
-                val p1 = Integer.parseInt(binding.etProduct1.text.toString())
-                val p2 = Integer.parseInt(binding.etProduct2.text.toString())
-                val p3 = Integer.parseInt(binding.etProduct3.text.toString())
-                val p4 = Integer.parseInt(binding.etProduct4.text.toString())
-                val p5 = Integer.parseInt(binding.etProduct5.text.toString())
-                val p6 = Integer.parseInt(binding.etProduct6.text.toString())
-                val p7 = Integer.parseInt(binding.etProduct7.text.toString())
-                val p8 = Integer.parseInt(binding.etProduct8.text.toString())
-                val p9 = Integer.parseInt(binding.etProduct9.text.toString())
-                val p10 = Integer.parseInt(binding.etProduct10.text.toString())
-                val p11 = Integer.parseInt(binding.etProduct11.text.toString())
-                val p12 = Integer.parseInt(binding.etProduct12.text.toString())
-                val p13 = Integer.parseInt(binding.etProduct13.text.toString())
-                val p14 = Integer.parseInt(binding.etProduct14.text.toString())
-                val p15 = Integer.parseInt(binding.etProduct15.text.toString())
-
-               val totalAmount =(p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15)
-
-                dialog.findViewById<TextInputEditText>(R.id.et_product_expense).setText(totalAmount)
-
-                saveProductExpense.setOnClickListener {
-                    Toast.makeText(requireContext(), "${totalAmount}", Toast.LENGTH_SHORT).show()
-                    uploadProductDataToDatabase()
-                }
-            }
-
-
-            dialog.show()
         }
 
         return binding.root
     }
 
+
+
+    private fun uploadProductDataToDatabase() = if (
+        binding.etProductAmount1.text!!.isNotEmpty() &&
+        binding.etProductAmount2.text!!.isNotEmpty() &&
+        binding.etProductAmount3.text!!.isNotEmpty() &&
+        binding.etProductAmount4.text!!.isNotEmpty() &&
+        binding.etProductAmount5.text!!.isNotEmpty() &&
+        binding.etProductAmount6.text!!.isNotEmpty() &&
+        binding.etProductAmount7.text!!.isNotEmpty() &&
+        binding.etProductAmount8.text!!.isNotEmpty() &&
+        binding.etProductAmount9.text!!.isNotEmpty() &&
+        binding.etProductAmount10.text!!.isNotEmpty() &&
+        binding.etProductAmount11.text!!.isNotEmpty() &&
+        binding.etProductAmount12.text!!.isNotEmpty() &&
+        binding.etProductAmount13.text!!.isNotEmpty() &&
+        binding.etProductAmount14.text!!.isNotEmpty() &&
+        binding.etProductAmount15.text!!.isNotEmpty()
+
+    ) {
+        //dialog creation
+        binding.btnContinue.setOnClickListener {
+            //calculating total
+            val p1 = parseInt(binding.etProductAmount1.text.toString())
+            val p2 = parseInt(binding.etProductAmount2.text.toString())
+            val p3 = parseInt(binding.etProductAmount3.text.toString())
+            val p4 = parseInt(binding.etProductAmount4.text.toString())
+            val p5 = parseInt(binding.etProductAmount5.text.toString())
+            val p6 = parseInt(binding.etProductAmount6.text.toString())
+            val p7 = parseInt(binding.etProductAmount7.text.toString())
+            val p8 = parseInt(binding.etProductAmount8.text.toString())
+            val p9 = parseInt(binding.etProductAmount9.text.toString())
+            val p10 = parseInt(binding.etProductAmount10.text.toString())
+            val p11 = parseInt(binding.etProductAmount11.text.toString())
+            val p12 = parseInt(binding.etProductAmount12.text.toString())
+            val p13 = parseInt(binding.etProductAmount13.text.toString())
+            val p14 = parseInt(binding.etProductAmount14.text.toString())
+            val p15 = parseInt(binding.etProductAmount15.text.toString())
+
+            val totalAmount =
+                (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15)
+
+            Toast.makeText(requireContext(), "$totalAmount", Toast.LENGTH_SHORT).show()
+
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.item_expense_dialog)
+            dialog.setCancelable(false)
+            val dismissDialog = dialog.findViewById<Button>(R.id.btn_cancel_dialog)
+            dismissDialog.setOnClickListener {
+                dialog.dismiss()
+            }
+            //set product total amount to cc expense
+            CC = dialog.findViewById(R.id.et_current_cash_expense)
+            CC.setText(totalAmount.toString())
+            //set home cash and banking
+            HC = dialog.findViewById(R.id.et_home_cash_expense)
+            BK = dialog.findViewById(R.id.et_online_banking)
+
+            //set total amount
+            val makeTotal = dialog.findViewById<Button>(R.id.btn_make_total)
+            makeTotal.setOnClickListener {
+                val CC = parseInt(CC.text.toString()) //current cash
+                val BC =
+                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_online_banking).text.toString())//banking cash
+                val SC =
+                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
+
+                val dailyTotal = (CC + BC + SC)
+                Toast.makeText(requireContext(), "daily total : $dailyTotal", Toast.LENGTH_SHORT)
+                    .show()
+                DT = dialog.findViewById(R.id.et_daily_total)
+                DT.setText(dailyTotal.toString())
+            }
+            //set 5% percent
+            val makeFivePer = dialog.findViewById<Button>(R.id.btn_make_five_per)
+            makeFivePer.setOnClickListener {
+                val dt = dialog.findViewById(R.id.et_daily_total) as TextInputEditText
+                val DT = parseInt(dt.text.toString())
+                val fivePercent = ((DT / 100) * 5)
+                Toast.makeText(requireContext(), "daily total : $fivePercent", Toast.LENGTH_SHORT)
+                    .show()
+                DD = dialog.findViewById(R.id.et_five_per) //five percent
+                DD.setText(fivePercent.toString())
+            }
+            //set cash in will became next home cash
+            val makeCashIn = dialog.findViewById<Button>(R.id.btn_make_cash_in)
+            makeCashIn.setOnClickListener {
+                val SC =
+                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
+                val dt =
+                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_five_per).text.toString()) //daily total
+                val cashIn = (SC - dt)
+                Toast.makeText(requireContext(), "daily total : $cashIn", Toast.LENGTH_SHORT).show()
+                CI = dialog.findViewById(R.id.et_cash_in)
+                CI.setText(cashIn.toString())
+            }
+            val saveDailyExpense = dialog.findViewById<Button>(R.id.btn_save_expense)
+            dialog.show()
+
+            saveDailyExpense.setOnClickListener {
+                clearAllFields()
+                dialog.dismiss()
+            }
+            //saveDateToDatabase()
+
+        }
+
+    } else {
+        Toast.makeText(requireContext(), "Enter 00 in empty filed", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveDateToDatabase() {
+
+    }
 
     private fun clearAllFields() {
 
@@ -113,38 +196,8 @@ class ExpenseFragment : Fragment() {
         binding.etProductAmount14.setText("")
         binding.etProductAmount15.setText("")
 
-        Toast.makeText(requireContext(), "All filed clear \n Insert Data Again", Toast.LENGTH_SHORT)
+        Toast.makeText(requireContext(), "All filed clear Insert Data Again", Toast.LENGTH_SHORT)
             .show()
-    }
-
-    private fun uploadProductDataToDatabase() {
-        if (
-            binding.etProductAmount1.text!!.isNotEmpty() &&
-            binding.etProductAmount2.text!!.isNotEmpty() &&
-            binding.etProductAmount3.text!!.isNotEmpty() &&
-            binding.etProductAmount4.text!!.isNotEmpty() &&
-            binding.etProductAmount5.text!!.isNotEmpty() &&
-            binding.etProductAmount6.text!!.isNotEmpty() &&
-            binding.etProductAmount7.text!!.isNotEmpty() &&
-            binding.etProductAmount8.text!!.isNotEmpty() &&
-            binding.etProductAmount9.text!!.isNotEmpty() &&
-            binding.etProductAmount10.text!!.isNotEmpty() &&
-            binding.etProductAmount11.text!!.isNotEmpty() &&
-            binding.etProductAmount12.text!!.isNotEmpty() &&
-            binding.etProductAmount13.text!!.isNotEmpty() &&
-            binding.etProductAmount14.text!!.isNotEmpty() &&
-            binding.etProductAmount15.text!!.isNotEmpty()
-
-        ) {
-            Toast.makeText(
-                requireContext(),
-                "data ready to insert ${binding.etProduct1.text} /n ${binding.etProductAmount1.text.toString()}",
-                Toast.LENGTH_SHORT
-            ).show()
-
-        } else {
-            Toast.makeText(requireContext(), "Enter 00 in empty filed", Toast.LENGTH_SHORT).show()
-        }
     }
 
 }
