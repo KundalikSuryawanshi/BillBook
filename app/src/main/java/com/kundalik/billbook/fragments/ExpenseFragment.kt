c28dae6d4622040f3b6e328b84d66f774094bcc8
kundalik_suryawanshi <kundaliksofficial@gmail.com>
2023-02-06 19:23:27 +0530

    upload-product-details-to-firestore

--- app/src/main/java/com/kundalik/billbook/fragments/ExpenseFragment.kt
+++ app/src/main/java/com/kundalik/billbook/fragments/ExpenseFragment.kt
@@ -1,27 +1,33 @@
 package com.kundalik.billbook.fragments
 
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.kundalik.billbook.MainActivity
 import com.kundalik.billbook.R
 import com.kundalik.billbook.databinding.FragmentExpenseBinding
+import com.kundalik.billbook.model.Expense
 import java.lang.Integer.parseInt
+import java.util.Calendar
+import java.util.Locale
 
 
 class ExpenseFragment : Fragment() {
 
     private lateinit var binding: FragmentExpenseBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var firestore: FirebaseFirestore
 
     //fields variables
     private lateinit var CC: TextInputEditText //current cash expense
@@ -37,9 +43,18 @@ class ExpenseFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentExpenseBinding.inflate(layoutInflater)
+        database = FirebaseDatabase.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR).toString()
+        val month = c.get(Calendar.MONTH).toString()
+        val day = c.get(Calendar.DAY_OF_MONTH).toString()
+        val date = c.get(Calendar.DATE).toString()
 
         val product = resources.getStringArray(R.array.products)
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdownitem, product)
+
         binding.etProduct1.setAdapter(arrayAdapter)
         binding.etProduct2.setAdapter(arrayAdapter)
         binding.etProduct3.setAdapter(arrayAdapter)
@@ -56,127 +71,239 @@ class ExpenseFragment : Fragment() {
         binding.etProduct14.setAdapter(arrayAdapter)
         binding.etProduct15.setAdapter(arrayAdapter)
 
-        binding.btnContinue.setOnClickListener {
-            uploadProductDataToDatabase()
+        binding.btnSaveProduct.setOnClickListener {
+            uploadProductDataToFireStore()
         }
-
+        
+        binding.btnContinue.setOnClickListener {
+            makeCalculations()
+        }
+        
         return binding.root
     }
 
+    private fun uploadProductDataToFireStore() {
+
+        val p1 = parseInt(binding.etProductAmount1.text.toString())
+        val p2 = parseInt(binding.etProductAmount2.text.toString())
+        val p3 = parseInt(binding.etProductAmount3.text.toString())
+        val p4 = parseInt(binding.etProductAmount4.text.toString())
+        val p5 = parseInt(binding.etProductAmount5.text.toString())
+        val p6 = parseInt(binding.etProductAmount6.text.toString())
+        val p7 = parseInt(binding.etProductAmount7.text.toString())
+        val p8 = parseInt(binding.etProductAmount8.text.toString())
+        val p9 = parseInt(binding.etProductAmount9.text.toString())
+        val p10 = parseInt(binding.etProductAmount10.text.toString())
+        val p11 = parseInt(binding.etProductAmount11.text.toString())
+        val p12 = parseInt(binding.etProductAmount12.text.toString())
+        val p13 = parseInt(binding.etProductAmount13.text.toString())
+        val p14 = parseInt(binding.etProductAmount14.text.toString())
+        val p15 = parseInt(binding.etProductAmount15.text.toString())
+
+        //upload product details to database
+        //logic to save data
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR).toString()
+        val month = c.get(Calendar.MONTH).toString()
+        val day = c.get(Calendar.DAY_OF_MONTH).toString()
+        val date = c.get(Calendar.DATE).toString()
+
+        //products
+        val productMap = mutableMapOf<String, Int>()
+
+        productMap.put(binding.etProduct1.text!!.toString(), p1)
+        productMap.put(binding.etProduct2.text!!.toString(), p2)
+        productMap.put(binding.etProduct3.text!!.toString(), p3)
+        productMap.put(binding.etProduct4.text!!.toString(), p4)
+        productMap.put(binding.etProduct5.text!!.toString(), p5)
+        productMap.put(binding.etProduct6.text!!.toString(), p6)
+        productMap.put(binding.etProduct7.text!!.toString(), p7)
+        productMap.put(binding.etProduct8.text!!.toString(), p8)
+        productMap.put(binding.etProduct9.text!!.toString(), p9)
+        productMap.put(binding.etProduct10.text!!.toString(), p10)
+        productMap.put(binding.etProduct11.text!!.toString(), p11)
+        productMap.put(binding.etProduct12.text!!.toString(), p12)
+        productMap.put(binding.etProduct13.text!!.toString(), p13)
+        productMap.put(binding.etProduct14.text!!.toString(), p14)
+        productMap.put(binding.etProduct15.text!!.toString(), p15)
 
 
-    private fun uploadProductDataToDatabase() = if (
-        binding.etProductAmount1.text!!.isNotEmpty() &&
-        binding.etProductAmount2.text!!.isNotEmpty() &&
-        binding.etProductAmount3.text!!.isNotEmpty() &&
-        binding.etProductAmount4.text!!.isNotEmpty() &&
-        binding.etProductAmount5.text!!.isNotEmpty() &&
-        binding.etProductAmount6.text!!.isNotEmpty() &&
-        binding.etProductAmount7.text!!.isNotEmpty() &&
-        binding.etProductAmount8.text!!.isNotEmpty() &&
-        binding.etProductAmount9.text!!.isNotEmpty() &&
-        binding.etProductAmount10.text!!.isNotEmpty() &&
-        binding.etProductAmount11.text!!.isNotEmpty() &&
-        binding.etProductAmount12.text!!.isNotEmpty() &&
-        binding.etProductAmount13.text!!.isNotEmpty() &&
-        binding.etProductAmount14.text!!.isNotEmpty() &&
-        binding.etProductAmount15.text!!.isNotEmpty()
 
-    ) {
-        //dialog creation
-        binding.btnContinue.setOnClickListener {
-            //calculating total
-            val p1 = parseInt(binding.etProductAmount1.text.toString())
-            val p2 = parseInt(binding.etProductAmount2.text.toString())
-            val p3 = parseInt(binding.etProductAmount3.text.toString())
-            val p4 = parseInt(binding.etProductAmount4.text.toString())
-            val p5 = parseInt(binding.etProductAmount5.text.toString())
-            val p6 = parseInt(binding.etProductAmount6.text.toString())
-            val p7 = parseInt(binding.etProductAmount7.text.toString())
-            val p8 = parseInt(binding.etProductAmount8.text.toString())
-            val p9 = parseInt(binding.etProductAmount9.text.toString())
-            val p10 = parseInt(binding.etProductAmount10.text.toString())
-            val p11 = parseInt(binding.etProductAmount11.text.toString())
-            val p12 = parseInt(binding.etProductAmount12.text.toString())
-            val p13 = parseInt(binding.etProductAmount13.text.toString())
-            val p14 = parseInt(binding.etProductAmount14.text.toString())
-            val p15 = parseInt(binding.etProductAmount15.text.toString())
+        firestore.collection(year).document(month).collection(date).document(day)
+            .set(productMap)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "success", Toast.LENGTH_SHORT).show()
 
-            val totalAmount =
-                (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15)
-
-            Toast.makeText(requireContext(), "$totalAmount", Toast.LENGTH_SHORT).show()
-
-            val dialog = Dialog(requireContext())
-            dialog.setContentView(R.layout.item_expense_dialog)
-            dialog.setCancelable(false)
-            val dismissDialog = dialog.findViewById<Button>(R.id.btn_cancel_dialog)
-            dismissDialog.setOnClickListener {
-                dialog.dismiss()
             }
-            //set product total amount to cc expense
-            CC = dialog.findViewById(R.id.et_current_cash_expense)
-            CC.setText(totalAmount.toString())
-            //set home cash and banking
-            HC = dialog.findViewById(R.id.et_home_cash_expense)
-            BK = dialog.findViewById(R.id.et_online_banking)
-
-            //set total amount
-            val makeTotal = dialog.findViewById<Button>(R.id.btn_make_total)
-            makeTotal.setOnClickListener {
-                val CC = parseInt(CC.text.toString()) //current cash
-                val BC =
-                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_online_banking).text.toString())//banking cash
-                val SC =
-                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
-
-                val dailyTotal = (CC + BC + SC)
-                Toast.makeText(requireContext(), "daily total : $dailyTotal", Toast.LENGTH_SHORT)
+            .addOnFailureListener {
+                Toast.makeText(
+                    requireContext(),
+                    "fail ${it.message}",
+                    Toast.LENGTH_SHORT
+                )
                     .show()
-                DT = dialog.findViewById(R.id.et_daily_total)
-                DT.setText(dailyTotal.toString())
             }
-            //set 5% percent
-            val makeFivePer = dialog.findViewById<Button>(R.id.btn_make_five_per)
-            makeFivePer.setOnClickListener {
-                val dt = dialog.findViewById(R.id.et_daily_total) as TextInputEditText
-                val DT = parseInt(dt.text.toString())
-                val fivePercent = ((DT / 100) * 5)
-                Toast.makeText(requireContext(), "daily total : $fivePercent", Toast.LENGTH_SHORT)
-                    .show()
-                DD = dialog.findViewById(R.id.et_five_per) //five percent
-                DD.setText(fivePercent.toString())
-            }
-            //set cash in will became next home cash
-            val makeCashIn = dialog.findViewById<Button>(R.id.btn_make_cash_in)
-            makeCashIn.setOnClickListener {
-                val SC =
-                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
-                val dt =
-                    parseInt(dialog.findViewById<TextInputEditText>(R.id.et_five_per).text.toString()) //daily total
-                val cashIn = (SC - dt)
-                Toast.makeText(requireContext(), "daily total : $cashIn", Toast.LENGTH_SHORT).show()
-                CI = dialog.findViewById(R.id.et_cash_in)
-                CI.setText(cashIn.toString())
-            }
-            val saveDailyExpense = dialog.findViewById<Button>(R.id.btn_save_expense)
-            dialog.show()
+    }
 
-            saveDailyExpense.setOnClickListener {
-                clearAllFields()
-                dialog.dismiss()
-            }
-            //saveDateToDatabase()
+    private fun makeCalculations() {
 
+
+        if (
+            binding.etProductAmount1.text!!.isNotEmpty() &&
+            binding.etProductAmount2.text!!.isNotEmpty() &&
+            binding.etProductAmount3.text!!.isNotEmpty() &&
+            binding.etProductAmount4.text!!.isNotEmpty() &&
+            binding.etProductAmount5.text!!.isNotEmpty() &&
+            binding.etProductAmount6.text!!.isNotEmpty() &&
+            binding.etProductAmount7.text!!.isNotEmpty() &&
+            binding.etProductAmount8.text!!.isNotEmpty() &&
+            binding.etProductAmount9.text!!.isNotEmpty() &&
+            binding.etProductAmount10.text!!.isNotEmpty() &&
+            binding.etProductAmount11.text!!.isNotEmpty() &&
+            binding.etProductAmount12.text!!.isNotEmpty() &&
+            binding.etProductAmount13.text!!.isNotEmpty() &&
+            binding.etProductAmount14.text!!.isNotEmpty() &&
+            binding.etProductAmount15.text!!.isNotEmpty()
+
+        ) {
+
+            binding.btnContinue.setOnClickListener {
+
+                val dialog = Dialog(requireContext())
+                dialog.setContentView(R.layout.item_expense_dialog)
+                dialog.setCancelable(false)
+                val dismissDialog = dialog.findViewById<Button>(R.id.btn_cancel_dialog)
+                dismissDialog.setOnClickListener {
+                    dialog.dismiss()
+                }
+
+                //calculating total
+                val p1 = parseInt(binding.etProductAmount1.text.toString())
+                val p2 = parseInt(binding.etProductAmount2.text.toString())
+                val p3 = parseInt(binding.etProductAmount3.text.toString())
+                val p4 = parseInt(binding.etProductAmount4.text.toString())
+                val p5 = parseInt(binding.etProductAmount5.text.toString())
+                val p6 = parseInt(binding.etProductAmount6.text.toString())
+                val p7 = parseInt(binding.etProductAmount7.text.toString())
+                val p8 = parseInt(binding.etProductAmount8.text.toString())
+                val p9 = parseInt(binding.etProductAmount9.text.toString())
+                val p10 = parseInt(binding.etProductAmount10.text.toString())
+                val p11 = parseInt(binding.etProductAmount11.text.toString())
+                val p12 = parseInt(binding.etProductAmount12.text.toString())
+                val p13 = parseInt(binding.etProductAmount13.text.toString())
+                val p14 = parseInt(binding.etProductAmount14.text.toString())
+                val p15 = parseInt(binding.etProductAmount15.text.toString())
+
+                val totalAmount =
+                    (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15)
+                Toast.makeText(requireContext(), "$totalAmount", Toast.LENGTH_SHORT).show()
+
+                //dialog creation
+
+                //set product total amount to cc expense
+                CC = dialog.findViewById(R.id.et_current_cash_expense)
+                CC.setText(totalAmount.toString())
+                //set home cash and banking
+                HC = dialog.findViewById(R.id.et_home_cash_expense)
+                BK = dialog.findViewById(R.id.et_online_banking)
+
+                //set total amount
+                val makeTotal = dialog.findViewById<Button>(R.id.btn_make_total)
+                makeTotal.setOnClickListener {
+                    val CC = parseInt(CC.text.toString()) //current cash
+                    val BC =
+                        parseInt(dialog.findViewById<TextInputEditText>(R.id.et_online_banking).text.toString())//banking cash
+                    val SC =
+                        parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
+
+                    val dailyTotal = (CC + BC + SC)
+                    Toast.makeText(
+                        requireContext(),
+                        "daily total : $dailyTotal",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    DT = dialog.findViewById(R.id.et_daily_total)
+                    DT.setText(dailyTotal.toString())
+                }
+                //set 5% percent
+                val makeFivePer = dialog.findViewById<Button>(R.id.btn_make_five_per)
+                makeFivePer.setOnClickListener {
+                    val dt = dialog.findViewById(R.id.et_daily_total) as TextInputEditText
+                    val DT = parseInt(dt.text.toString())
+                    val fivePercent = ((DT / 100) * 5)
+                    Toast.makeText(
+                        requireContext(),
+                        "daily total : $fivePercent",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    DD = dialog.findViewById(R.id.et_five_per) //five percent
+                    DD.setText(fivePercent.toString())
+                }
+                //set cash in will became next home cash
+                val makeCashIn = dialog.findViewById<Button>(R.id.btn_make_cash_in)
+                makeCashIn.setOnClickListener {
+                    val SC =
+                        parseInt(dialog.findViewById<TextInputEditText>(R.id.et_shop_cash).text.toString()) //shop cash
+                    val dt =
+                        parseInt(dialog.findViewById<TextInputEditText>(R.id.et_five_per).text.toString()) //daily total
+                    val cashIn = (SC - dt)
+                    Toast.makeText(requireContext(), "daily total : $cashIn", Toast.LENGTH_SHORT)
+                        .show()
+                    CI = dialog.findViewById(R.id.et_cash_in)
+                    CI.setText(cashIn.toString())
+                }
+                val saveDailyExpense = dialog.findViewById<Button>(R.id.btn_save_expense)
+                
+                saveDailyExpense.setOnClickListener {
+
+                    val dialog2 = Dialog(requireContext())
+                    dialog.setContentView(R.layout.loading_layout)
+                    if (dialog.window != null) {
+                        dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+                    }
+                    dialog2.show()
+
+                    val c = Calendar.getInstance()
+                    val year = c.get(Calendar.YEAR).toString()
+                    val month = c.get(Calendar.MONTH).toString()
+                    val day = c.get(Calendar.DAY_OF_MONTH).toString()
+                    val date = c.get(Calendar.DATE).toString()
+                    
+                    val currentCash = dialog.findViewById<TextInputEditText>(R.id.et_current_cash_expense)
+                    val homeCash = dialog.findViewById<TextInputEditText>(R.id.et_home_cash_expense)
+                    val onlineBanking = dialog.findViewById<TextInputEditText>(R.id.et_online_banking)
+                    val shopExpense = dialog.findViewById<TextInputEditText>(R.id.et_shop_cash)
+                    val fivePercent = dialog.findViewById<TextInputEditText>(R.id.et_five_per)
+                    val cashIn = dialog.findViewById<TextInputEditText>(R.id.et_cash_in)
+
+                    val expense = Expense(currentCash, homeCash, onlineBanking, shopExpense, fivePercent, cashIn)
+
+                    database.reference.child("Expenses").child("year: ${year}").child("month:${month}").child("date:${date}")
+                        .setValue(expense)
+                        .addOnSuccessListener {
+                            Toast.makeText(requireContext(), "success", Toast.LENGTH_SHORT).show()
+                            dialog2.dismiss()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(requireContext(), "Error ${it.message}", Toast.LENGTH_SHORT)
+                                .show()
+                            dialog2.dismiss()
+                        }
+
+
+                }
+                dialog.show()
+                
+            }
+
+        } else {
+            Toast.makeText(requireContext(), "Enter 00 in empty filed", Toast.LENGTH_SHORT).show()
         }
 
-    } else {
-        Toast.makeText(requireContext(), "Enter 00 in empty filed", Toast.LENGTH_SHORT).show()
     }
 
-    private fun saveDateToDatabase() {
-
-    }
 
     private fun clearAllFields() {
 
--- app/src/main/java/com/kundalik/billbook/model/Expense.kt
+++ app/src/main/java/com/kundalik/billbook/model/Expense.kt
@@ -1,5 +1,20 @@
 package com.kundalik.billbook.model
 
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.kundalik.billbook.MainActivity
+import com.kundalik.billbook.R
+
 data class Expense(
-    val id : Int
+    val currentCashExpense: TextInputEditText,
+    val homeCashExpense:TextInputEditText,
+    val onlineBanking:TextInputEditText,
+    val shopCashExpense: TextInputEditText,
+    val fivePercent: TextInputEditText,
+    val cashIn: TextInputEditText
 )
+
+
