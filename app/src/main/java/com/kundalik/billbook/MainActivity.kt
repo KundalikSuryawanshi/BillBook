72e3cc389daa65006a40a1699201c3064f44379c
kundalik_suryawanshi <kundaliksofficial@gmail.com>
2023-02-12 23:00:56 +0530

    push-notification_implement

--- app/src/main/java/com/kundalik/billbook/MainActivity.kt
+++ app/src/main/java/com/kundalik/billbook/MainActivity.kt
@@ -3,8 +3,11 @@ package com.kundalik.billbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessaging
 import com.kundalik.billbook.adapter.ViewPagerAdapter
 import com.kundalik.billbook.auth.LoginActivity
 import com.kundalik.billbook.auth.MobileNumberActivity
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseMessaging: FirebaseMessaging
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,6 +31,8 @@ class MainActivity : AppCompatActivity() {
         checkUserNullOrNot()
 
         setUpViewPagerAdapter()
+
+        getFCMToken()
 
     }
 
@@ -48,5 +55,18 @@ class MainActivity : AppCompatActivity() {
         binding.viewPager.adapter = adapter
         binding.tbLayout.setupWithViewPager(binding.viewPager)
 
+    }
+    
+    private fun getFCMToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Toast.makeText(this, "Failed to get FCM token", Toast.LENGTH_SHORT).show()
+                return@addOnCompleteListener
+            }
+            //get new fcm token
+            val token = it.result
+            Log.d("FCMTOKEN", token)
+        }
+        
     }
 }
